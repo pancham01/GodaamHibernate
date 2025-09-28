25fb828aac2386e7ba9b216493dc2a61a0077ba1
pancham01 <pancham9557@gmail.com>
2025-09-28 09:57:38 +0530

    update

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -16,46 +16,6 @@ public class Main {
 		Transaction tx = session.beginTransaction();
 		session.persist(e);
 		tx.commit();
-
-//		Employee employee = new Employee();
-//		session.load(employee, 4);
-//		Employee employee2 = session.find(Employee.class, 1);
-//		System.out.println(employee);
-//		System.out.println(employee2);
-		
-//		Select all records
-//		Query query = session.createQuery("from Employee",Employee.class);
-//		List list = query.list();
-//		System.out.println(list);
-		
-		
-//		Query query = session.createQuery("from Employee",Employee.class);
-//		query.setFirstResult(3);
-//		query.setMaxResults(5);
-//		List list = query.list();
-//		System.out.println(list);
-		
-//		Query query = session.createQuery("update Employee set name = :n where id = :i");
-//		query.setParameter("n", "Udit narayan");
-//		query.setParameter("i", 9);
-//		query.executeUpdate();
-		
-		
-		
-//		Query query = session.createQuery("delete from Employee where id = :i");
-//		query.setParameter("i", 9);
-//		query.executeUpdate();
-		
-		
-//		Query query = session.createQuery("select  max(name) from Employee");
-//		System.out.println(query.list());
-	
-		
-//		Query namedQuery = session.createNamedQuery("findEmps");
-//		List list = namedQuery.list();
-//		
-//		System.out.println(list);
-//		tx.commit();
 		System.out.println("Main.main()");
 
 	}
